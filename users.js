b9ac1677473ca80b1e0924518287de1ca9e4585c
ЕЕнот / RRaccoon <128936608+eenot-eenot@users.noreply.github.com>
2025-09-01 18:06:36 +0300

    Update users data via admin panel

--- users.js
+++ users.js
@@ -1,32 +1,46 @@
 const users = {
-    eenot: {
-        id: 'eenot',
-        discord: 'ЕЕнот / RRaccoon',
-        discordid: '794675642037567498',
-        gamename: 'eenot',
-        awards: ['test'],
-        events: 5,
+    "eenot": {
+        "id": "eenot",
+        "discord": "ЕЕнот / RRaccoon",
+        "discordid": "794675642037567498",
+        "gamename": "eenot",
+        "awards": [
+            "test1"
+        ],
+        "events": 5
     },
-    trid: {
-        id: 'trid',
-        discord: 'Trid',
-        discordid: '993585215358386306',
-        gamename: '3d',
-        awards: ['test'],
-        events: 10,
+    "trid": {
+        "id": "trid",
+        "discord": "Trid",
+        "discordid": "993585215358386306",
+        "gamename": "3d",
+        "awards": [
+            "test1"
+        ],
+        "events": 10
     },
+    "new_user_1756739180782": {
+        "id": "new_user_1756739180782",
+        "discord": "testuser",
+        "discordid": "00000000",
+        "gamename": "gamename",
+        "awards": [
+            "test1"
+        ],
+        "events": "999"
+    }
 };
 
 const awards = {
-    test1: {
-        name: 'Тест',
-        img: 'test.png',
-        type: 'best',
-    },
+    "test1": {
+        "name": "Тест",
+        "img": "test.png",
+        "type": "best"
+    }
 };
 
 const types = {
-    best: 5,
-    great: 3,
-    small: 1
-}
+    "best": 5,
+    "great": 3,
+    "small": 1
+};
