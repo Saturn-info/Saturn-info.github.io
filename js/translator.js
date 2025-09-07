800464da62a4824032e540e0ae010e2f83f16e21
EEHOT <i.is.raccoon228@gmail.com>
2025-09-07 23:32:20 +0300

    Donate info

--- js/translator.js
+++ js/translator.js
@@ -10,6 +10,8 @@ class LangMgr {
             "en": {
                 "leaderboard": "Leaderboard",
                 "discord": "Discord",
+                "donate": "Donate Info",
+
                 "score": "Score",
                 "game": "Game-name",
                 "awards": "Awards",
@@ -25,10 +27,28 @@ class LangMgr {
                 "eventDT": "Event:",
                 "typeDT": "Type:",
                 "scoreDT": "Score:",
+                "donatetext": "<div style='display: inline-flex'><p>Not anavible now for Western cards. For read about how to pay using Russian card <i onclick='window.langMgr.setLang(`ru`)' style='color: #b62fe8; cursor: pointer;'>change language</i></p></div>",
+                "donateoptions": `
+<h2>Elite</h2>
+<b>Duration: </b>1 month<br>
+<b>Cost: </b>1 USD/100 RUR<br>
+<b>Features:</b><br>
+1. Cool prefix<br>
+2. /civ command<br>
+3. Special role in Discord<br>
+4. Elite category in Discord<br>
+5. Commands for customizing the country<br>
+6. /content command and viewing all scenarios and maps<br>
+7. Permission to write guides in the Discord server<br>
+8. Access to the great server archive<br>
+<br>
+<i>Pay using western cards coming soon... All information about payment from Russian cards here: <a href="#donateru" onclick="document.getElementById('donateru').classList.add('active')">click</a></i>`,
             },
             "ru": {
                 "leaderboard": "Доска лидеров",
                 "discord": "Discord",
+                "donate": "Информация о Донате",
+
                 "score": "Счёт",
                 "game": "Ник в игре",
                 "awards": "Медальки",
@@ -44,6 +64,39 @@ class LangMgr {
                 "eventDT": "Ивент:",
                 "typeDT": "Тип:",
                 "scoreDT": "Ценность:",
+
+                "donatetext": `
+<h2> Информация по заказу и оплате доната</h2>
+
+<b>Начните перевод нужной суммы для определённого товара на  карту: 2202208093100387</b><br>
+Карта Сбербанка, получатель Денис Д.<br>
+<br>
+При переводе, заполните сообщение получателю по данной форме, чтобы мы знали, кому и как выдавать товар:<br>
+1. Что покупаете<br>
+2. Ваше имя пользователя в Discord<br>
+3. Ваш ID в Warnament (чтобы узнать, напишите любое сообщение в чате, цифра у ника - ваш ID)<br>
+<br>
+<b>После выполнения всех указанных с верху действий, просто ждите вашу награду.</b><br>
+<br>
+<h2>Важно!</h2>
+Если вы пришлёте недостаточное количество средств для совершения покупки, мы не несём ответственность за возращение средств. Награда также не будет задействована. Пожалуйста, будьте внимательны при покупке!<br>
+<br>
+<i>При совершении доната вы не только получаете награду, но и сильно помогаете нам развивать проект.</i><br>`,
+                "donateoptions": `
+<h2>Elite</h2>
+<b>Продолжительность: </b>1 месяц<br>
+<b>Стоимость: </b>1 доллар/100 рублей<br>
+<b>Особенности:</b><br>
+1. Классный префикс<br>
+2. команда /civ<br>
+3. Особая роль в Discord<br>
+4. Элитная категория в Discord<br>
+5. Команды для настройки страны<br>
+6. Команда /content и просмотра всех сценариев и карт<br>
+7. Разрешение на написание руководств на сервере Discord<br>
+8. Доступ к большому архиву сервера<br>
+<br>
+<i>Вся информация об оплате с российских карт здесь: <a href="#donateru" onclick="document.getElementById('donateru').classList.add('active')">click</a></i>`,
             },
         };
         if (localStorage.getItem('SaturnLang')) {
@@ -73,7 +126,11 @@ class LangMgr {
             const key = el.getAttribute("data-lang");
             const translation = this.translations[this.currentLang]?.[key];
             if (translation) {
-                el.textContent = translation;
+                if (translation.includes('<')) {
+                    el.innerHTML = translation;
+                } else {
+                    el.textContent = translation;
+                }
             } else {
                 console.warn(`Нет перевода для ключа "${key}" на языке "${this.currentLang}"`);
             }
